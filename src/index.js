4f2d421b9311f24b435c073ca1e44c33e4afda66
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-09-13 15:30:25 -0500

    Add pseudocode for project

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// Create todo items with a factory
+// Assign todo to a list
+// todo should have title, descr, date, priority
+// todos should have a button to delete 
+// when delete todo remove from list
+
+// Users should be able to create new todo
+// Form for user to create new todo
+// todo should get added to current list
+
+// Add default list of todos
+// Load default list if no local storage
+
+// Users should be able to create a new list 
+// Form for users to create new list
+
+// Users should be able to swap lists 
+// Button to delete list 
+// When deleting list, delete all todos inside
+
+// UI
+// Header with 'To Do List'
+// Sidebar with Lists and button to create new list
+// Body with todos of current list and button to create new todo
+
+
+// Add local storage
+// Save lists and todos to local storage when new one is added
+// Look for data in local storage when loading
